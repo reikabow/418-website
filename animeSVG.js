0955d6ba9d96384298937f86db61fca89f584ecb
Tia <smithrtia@gmail.com>
2018-10-29 14:29:57 -0500

    a

--- animeSVG.js
+++ animeSVG.js
@@ -17,4 +17,18 @@ for (var i = 0; i < pathEls.length; i++) {
     easing: 'easeInOutSine',
     autoplay: true
   });
+
+
+	var colors = anime({
+		targets: '#colors .el',
+		translateX: 250,
+		backgroundColor: [
+		  {value: '#FFF'}, // Or #FFFFFF
+		  {value: 'rgb(255, 0, 0)'},
+		  {value: 'hsl(100, 60%, 60%)'}
+		],
+		easing: 'linear',
+		direction: 'alternate',
+		duration: 2000
+	  });
 }
